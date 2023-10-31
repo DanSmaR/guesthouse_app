9c05c5fd671df9fa1e4939be7190cbc4565bbf5f
Danilo Martins <dansm86@outlook.com>
2023-10-31 16:23:26 -0300

    test:guesthouse model unit testing

--- spec/models/guesthouse_spec.rb
+++ spec/models/guesthouse_spec.rb
@@ -1,5 +1,85 @@
 require 'rails_helper'
 
 RSpec.describe Guesthouse, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'false when corporate_name is empty' do
+      # Arrange
+      guesthouse = Guesthouse.new(corporate_name: '', brand_name: 'Pousada Nascer do Sol',
+                                  registration_code: '47032102000152', phone_number: '15983081833',
+                                  email: 'contato@email.com',
+                                  description: 'Pousada com vista linda para a serra',
+                                  payment_method: 0, pets: true,
+                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+      # Act
+      result = guesthouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when brand_name is empty' do
+      # Arrange
+      guesthouse = Guesthouse.new(corporate_name: 'Pousada Nascer do Sol LTDA.', brand_name: '',
+                                  registration_code: '47032102000152', phone_number: '15983081833',
+                                  email: 'contato@email.com',
+                                  description: 'Pousada com vista linda para a serra',
+                                  payment_method: 0, pets: true,
+                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+      # Act
+      result = guesthouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when registration_code is empty' do
+      # Arrange
+      guesthouse = Guesthouse.new(corporate_name: 'Pousada Nascer do Sol LTDA.', brand_name: 'Pousada Nascer do Sol',
+                                  registration_code: '', phone_number: '15983081833',
+                                  email: 'contato@email.com',
+                                  description: 'Pousada com vista linda para a serra',
+                                  payment_method: 0, pets: true,
+                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+      # Act
+      result = guesthouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when phone_number is empty' do
+      # Arrange
+      guesthouse = Guesthouse.new(corporate_name: 'Pousada Nascer do Sol LTDA.', brand_name: 'Pousada Nascer do Sol',
+                                  registration_code: '47032102000152', phone_number: '',
+                                  email: 'contato@email.com',
+                                  description: 'Pousada com vista linda para a serra',
+                                  payment_method: 0, pets: true,
+                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+      # Act
+      result = guesthouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when email is empty' do
+      # Arrange
+      guesthouse = Guesthouse.new(corporate_name: 'Pousada Nascer do Sol LTDA.', brand_name: 'Pousada Nascer do Sol',
+                                  registration_code: '47032102000152', phone_number: '15983081833',
+                                  email: '',
+                                  description: 'Pousada com vista linda para a serra',
+                                  payment_method: 0, pets: true,
+                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+      # Act
+      result = guesthouse.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  # pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it 'false when name is empty' do
+      # Arrange
+      user = User.new(name: '', email: 'contato@email.com',
+                      password: 'password')
+
+      # Act
+      result = user.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when email is empty' do
+      # Arrange
+      user = User.new(name: 'João', email: '',
+                      password: 'password')
+
+      # Act
+      result = user.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+
+    it 'false when password is empty' do
+      # Arrange
+      user = User.new(name: 'João', email: 'contato@email.com',
+                      password: '')
+
+      # Act
+      result = user.valid?
+
+      # Assert
+      expect(result).to eq false
+    end
+  end
 end
