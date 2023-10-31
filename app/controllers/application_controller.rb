b00ef9e9fe32496c70760a4b89696ef3b506fd36
Danilo Martins <dansm86@outlook.com>
2023-10-31 17:45:23 -0300

    feat:register user successfully
    added name to devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # call the configure_permitted_parameters method before any action in our application
+  # if the controller that the action is coming from is a devise_controller
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # permit the name attribute for the sign_up action
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
 end
