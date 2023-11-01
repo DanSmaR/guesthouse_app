8aabb17d355bdd87654b027c3f1ec50288cab120
Danilo Martins <dansm86@outlook.com>
2023-11-01 00:16:48 -0300

    test:redirect to a guesthouse register form page

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum role: { guest: 0, inn_owner: 1 }
+  enum role: { guest: 0, guesthouse_owner: 1 }
   has_one :guesthouse_owner
 end
--- spec/system/authentication/user_sign_up_spec.rb
+++ spec/system/authentication/user_sign_up_spec.rb
@@ -10,6 +10,7 @@ describe 'User signs up' do
     click_on 'Registrar-se'
     fill_in 'Nome', with: 'Maria'
     fill_in 'E-mail', with: 'maria@email.com'
+    select 'Proprietário de Pousada', from: 'Role'
     fill_in 'Senha', with: 'password'
     fill_in 'Confirme sua senha', with: 'password'
     click_on 'Criar conta'
@@ -19,5 +20,20 @@ describe 'User signs up' do
     expect(page).to have_content('maria@email.com')
     expect(page).to have_button('Sair')
     expect(User.last.name).to eq('Maria')
+  end
+
+  it 'as Guesthouse Owner' do
+    # Arrange
+
+    # Act
+    visit new_user_registration_path
+    fill_in 'E-mail', with: 'maria@email.com'
+    select 'Proprietário de Pousada', from: 'Role'
+    fill_in 'Senha', with: 'password'
+    fill_in 'Confirme sua senha', with: 'password'
+    click_on 'Criar conta'
+
+    # Assert
+    expect(current_path).to eq(new_guesthouse_path)
   end
 end
