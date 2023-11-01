4c21fe2b8fdefa7b444935511965f32f754c2fae
Danilo Martins <dansm86@outlook.com>
2023-11-01 10:58:46 -0300

    feat:added registration controller to routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -8,4 +7,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "home#index"
+
+  devise_for :users, :controllers => {:registrations => "registrations"}
+  resources :guesthouses, only: %i[new create]
 end
