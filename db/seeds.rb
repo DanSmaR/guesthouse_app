241ea238145b49ed7bb2a05132b1a1159d42550f
Danilo Martins <dansm86@outlook.com>
2023-11-01 18:37:02 -0300

    created seed to populate PaymentMethod model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# populate payment_methods table
+['credit_card', 'debit_card', 'pix'].each do |method|
+  PaymentMethod.find_or_create_by!(method: method)
+end
