1ec18e8132e96a231911b04e9d3c2823b6aae2c3
Danilo Martins <dansm86@outlook.com>
2023-11-01 22:31:58 -0300

    test: update guesthouse succesfully

--- spec/system/guesthouses/user_edit_guesthouse_spec.rb
+++ spec/system/guesthouses/user_edit_guesthouse_spec.rb
@@ -3,7 +3,6 @@ require 'rails_helper'
 describe 'User edit guesthouse' do
   it 'shows the forms edit page' do
     # Arrange
-    # Arrange
     user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
     guesthouse_owner = user.build_guesthouse_owner
     guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
@@ -53,4 +52,44 @@ describe 'User edit guesthouse' do
     expect(page).to have_field('Cartão de débito', type: 'checkbox', checked: false)
     expect(page).to have_field('Pix', type: 'checkbox', checked: false)
   end
+
+  it 'successfully edits a guesthouse' do
+    # Arrange
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
+    guesthouse_owner = user.build_guesthouse_owner
+    guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
+                                                   brand_name: 'Pousada Nascer do Sol',
+                                                   registration_code: '47032102000152',
+                                                   phone_number: '15983081833',
+                                                   email: 'contato@nascerdosol.com.br',
+                                                   description: 'Pousada com vista linda para a serra',
+                                                   pets: true,
+                                                   use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                   checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+    guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
+                             city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
+
+    PaymentMethod.create!(method: 'credit_card')
+    PaymentMethod.create!(method: 'debit_card')
+    PaymentMethod.create!(method: 'pix')
+
+    guesthouse.payment_methods = PaymentMethod.all
+    guesthouse.save!
+
+    # Act
+    login_as(user)
+    visit root_path
+    click_on 'Pousada Nascer do Sol'
+    click_on 'Editar Pousada'
+    fill_in 'Nome Fantasia', with: 'Pousada Pôr do Sol'
+    fill_in 'Descrição', with: 'Pousada com vista linda para o pôr do sol'
+    click_on 'Atualizar Pousada'
+
+    # Assert
+    expect(page).to have_content('Pousada Pôr do Sol')
+    expect(page).to have_content('Pousada com vista linda para o pôr do sol')
+    expect(page).to have_content('Pousada atualizada com sucesso')
+  end
+
+#   TODO - Add tests for empty fields
 end
