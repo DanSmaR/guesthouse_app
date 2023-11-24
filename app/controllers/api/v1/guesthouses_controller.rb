c66ee278533ead229e0e8fb0c4d61ecfa11bd098
Danilo Martins <dansm86@outlook.com>
2023-11-24 18:09:05 -0300

    feat:/api/v1/guesthouses/?search=ana endpoint

--- app/controllers/api/v1/guesthouses_controller.rb
+++ app/controllers/api/v1/guesthouses_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::GuesthousesController < Api::V1::ApiController
   def index
-    @guesthouse = Guesthouse.where(active: true)
-    render json: @guesthouse.as_json(except: [:created_at, :updated_at, :registration_code, :corporate_name],
+    guesthouses = Guesthouse.where(active: true)
+    if params[:search]
+      guesthouses = guesthouses.where('brand_name LIKE ?', "%#{params[:search]}%")
+
+    end
+    render json: guesthouses.as_json(except: [:created_at, :updated_at, :registration_code, :corporate_name],
                                      include: [:address, :payment_methods], status: :ok)
   end
 end
