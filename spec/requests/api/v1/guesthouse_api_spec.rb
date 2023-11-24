b4eb159b3813ce12fcdaf3670d42598332a933d3
Danilo Martins <dansm86@outlook.com>
2023-11-24 13:29:10 -0300

    fix:/api/v1/guesthouses endpoint
    changed to show list of guesthouses

--- spec/requests/api/v1/guesthouse_api_spec.rb
+++ spec/requests/api/v1/guesthouse_api_spec.rb
@@ -1,59 +1,101 @@
 require 'rails_helper'
 
 describe 'Guesthouse API', type: :request do
-  context 'GET /api/v1/guesthouses/1' do
-    it 'returns a guesthouse' do
+  context 'GET /api/v1/guesthouses' do
+    it 'returns a list of guesthouses' do
       # Arrange
-      user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
-      guesthouse_owner = user.build_guesthouse_owner
-      guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
-                                                     brand_name: 'Pousada Nascer do Sol',
-                                                     registration_code: '47032102000152',
-                                                     phone_number: '15983081833',
-                                                     email: 'contato@nascerdosol.com.br',
-                                                     description: 'Pousada com vista linda para a serra',
-                                                     pets: true,
-                                                     use_policy: 'Não é permitido fumar nas dependências da pousada',
-                                                     checkin_hour: '14:00', checkout_hour: '12:00', active: true)
-      guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
-                               city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
+      cities = %w[Itapetininga Camboriú]
+      states = %w[SP SC]
+      guesthouses_names = %w[Nascer\ do\ Sol Praiana]
+      user_names = %w[Joao Cesar]
+      guesthouse =  {
+        0 => '', 1 => ''
+      }
+      user = {
+        0 => '', 1 => ''
+      }
+      guesthouse_owner = {
+        0 => '', 1 => ''
+      }
 
       PaymentMethod.create!(method: 'credit_card')
       PaymentMethod.create!(method: 'debit_card')
       PaymentMethod.create!(method: 'pix')
 
-      guesthouse.payment_methods = PaymentMethod.all
-      guesthouse.save!
+      guesthouses_names.each_with_index do |name, index|
+        user[index] = User.create!(name: user_names[index], email: "#{user_names[index].downcase}@email.com",
+                                   password: 'password', role: 1)
+        guesthouse_owner[index] = user[index].build_guesthouse_owner
+        guesthouse[index] = guesthouse_owner[index].build_guesthouse(corporate_name: "#{name} LTDA.",
+                                                                     brand_name: "Pousada #{name}",
+                                                                     registration_code: "#{index}47032102000152",
+                                                                     phone_number: "#{index}1598308183",
+                                                                     email: "contato@#{name.downcase.gsub(" ", "")}.com.br",
+                                                                     description: "Descrição da Pousada #{name}",
+                                                                     pets: index.odd? ? true : false,
+                                                                     use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                                     checkin_hour: '14:00', checkout_hour: '12:00',
+                                                                     active: true)
+
+        guesthouse[index].build_address(street: index.odd? ? "Rua #{index},  #{index}000" : "Avenida #{index}, #{index}000",
+                                        neighborhood: index.even? ? "Bairro #{index}" : "Centro #{index}",
+                                        city: cities[index], state: states[index], postal_code: "#{index}1001-000")
+
+        guesthouse[index].payment_methods = PaymentMethod.all
+        guesthouse[index].save!
+      end
 
       # Act
-      get '/api/v1/guesthouses/1'
+      get '/api/v1/guesthouses'
 
       # Assert
       expect(response).to have_http_status(200)
       expect(response.content_type).to include('application/json')
 
       parsed_body = JSON.parse(response.body, symbolize_names: true)
-      expect(parsed_body[:corporate_name]).to eq('Pousada Nascer do Sol LTDA.')
-      expect(parsed_body[:brand_name]).to eq('Pousada Nascer do Sol')
-      expect(parsed_body[:registration_code]).to eq('47032102000152')
-      expect(parsed_body[:phone_number]).to eq('15983081833')
-      expect(parsed_body[:email]).to eq('contato@nascerdosol.com.br')
-      expect(parsed_body[:description]).to eq('Pousada com vista linda para a serra')
-      expect(parsed_body[:pets]).to eq(true)
-      expect(parsed_body[:use_policy]).to eq('Não é permitido fumar nas dependências da pousada')
-      expect(parsed_body[:checkin_hour]).to eq('14:00')
-      expect(parsed_body[:checkout_hour]).to eq('12:00')
-      expect(parsed_body[:active]).to eq(true)
-      expect(parsed_body[:address][:street]).to eq('Rua das Flores, 1000')
-      expect(parsed_body[:address][:neighborhood]).to eq('Vila Belo Horizonte')
-      expect(parsed_body[:address][:city]).to eq('Itapetininga')
-      expect(parsed_body[:address][:state]).to eq('SP')
-      expect(parsed_body[:address][:postal_code]).to eq('01001-000')
-      expect(parsed_body[:payment_methods][0][:method]).to eq('credit_card')
-      expect(parsed_body[:payment_methods][1][:method]).to eq('debit_card')
-      expect(parsed_body[:payment_methods][2][:method]).to eq('pix')
-      expect(parsed_body.keys).to_not include(:created_at)
-      expect(parsed_body.keys).to_not include(:updated_at)
+      expect(parsed_body[0][:corporate_name]).to_not eq('Pousada Nascer do Sol LTDA.')
+      expect(parsed_body[0][:brand_name]).to eq('Pousada Nascer do Sol')
+      expect(parsed_body[0][:registration_code]).to_not eq('47032102000152')
+      expect(parsed_body[0][:phone_number]).to eq('01598308183')
+      expect(parsed_body[0][:email]).to eq('contato@nascerdosol.com.br')
+      expect(parsed_body[0][:description]).to eq('Descrição da Pousada Nascer do Sol')
+      expect(parsed_body[0][:pets]).to eq(false)
+      expect(parsed_body[0][:use_policy]).to eq('Não é permitido fumar nas dependências da pousada')
+      expect(DateTime.parse(parsed_body[0][:checkin_hour])).to eq(guesthouse[0].checkin_hour)
+      expect(DateTime.parse(parsed_body[0][:checkout_hour])).to eq(guesthouse[0].checkout_hour)
+      expect(parsed_body[0][:active]).to eq(true)
+      expect(parsed_body[0][:address][:street]).to eq('Avenida 0, 0000')
+      expect(parsed_body[0][:address][:neighborhood]).to eq('Bairro 0')
+      expect(parsed_body[0][:address][:city]).to eq('Itapetininga')
+      expect(parsed_body[0][:address][:state]).to eq('SP')
+      expect(parsed_body[0][:address][:postal_code]).to eq('01001-000')
+      expect(parsed_body[0][:payment_methods][0][:method]).to eq('credit_card')
+      expect(parsed_body[0][:payment_methods][1][:method]).to eq('debit_card')
+      expect(parsed_body[0][:payment_methods][2][:method]).to eq('pix')
+      expect(parsed_body[0].keys).to_not include(:created_at)
+      expect(parsed_body[0].keys).to_not include(:updated_at)
+
+      expect(parsed_body[1][:corporate_name]).to_not eq('Pousada Praiana LTDA.')
+      expect(parsed_body[1][:brand_name]).to eq('Pousada Praiana')
+      expect(parsed_body[1][:registration_code]).to_not eq('147032102000152')
+      expect(parsed_body[1][:phone_number]).to eq('11598308183')
+      expect(parsed_body[1][:email]).to eq('contato@praiana.com.br')
+      expect(parsed_body[1][:description]).to eq('Descrição da Pousada Praiana')
+      expect(parsed_body[1][:pets]).to eq(true)
+      expect(parsed_body[1][:use_policy]).to eq('Não é permitido fumar nas dependências da pousada')
+      expect(DateTime.parse(parsed_body[1][:checkin_hour])).to eq(guesthouse[1].checkin_hour)
+      expect(DateTime.parse(parsed_body[1][:checkout_hour])).to eq(guesthouse[1].checkout_hour)
+      expect(parsed_body[1][:active]).to eq(true)
+      expect(parsed_body[1][:address][:street]).to eq('Rua 1,  1000')
+      expect(parsed_body[1][:address][:neighborhood]).to eq('Centro 1')
+      expect(parsed_body[1][:address][:city]).to eq('Camboriú')
+      expect(parsed_body[1][:address][:state]).to eq('SC')
+      expect(parsed_body[1][:address][:postal_code]).to eq('11001-000')
+      expect(parsed_body[1][:payment_methods][0][:method]).to eq('credit_card')
+      expect(parsed_body[1][:payment_methods][1][:method]).to eq('debit_card')
+      expect(parsed_body[1][:payment_methods][2][:method]).to eq('pix')
+      expect(parsed_body[1].keys).to_not include(:created_at)
+      expect(parsed_body[1].keys).to_not include(:updated_at)
     end
   end
 end
