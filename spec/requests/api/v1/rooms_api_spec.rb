81ab840df7f3f7f91c81e4a9e04892e640fa4dd6
Danilo Martins <dansm86@outlook.com>
2023-11-25 16:47:03 -0300

    test: GET /api/v1/rooms/1/availability

--- spec/requests/api/v1/rooms_api_spec.rb
+++ spec/requests/api/v1/rooms_api_spec.rb
@@ -168,80 +168,89 @@ describe 'Rooms API', type: :request do
     end
   end
 
-  context 'GET /api/v1/guesthouses/1/rooms/1/?check_in_date=' do
-    it 'returns a guesthouse' do
-      # Arrange
-      cities = %w[Itapetininga Sorocaba Camboriú]
-      states = %w[SP SP SC]
-      guesthouses_names = %w[Nascer\ do\ Sol Sorocabana Praiana]
-      user_names = %w[Joao Marlene Cesar]
-      guesthouse =  {
-        0 => '', 1 => ''
-      }
-      user = {
-        0 => '', 1 => ''
-      }
-      guesthouse_owner = {
-        0 => '', 1 => ''
-      }
+  context 'GET /api/v1/rooms/:id/availability' do
+    context 'when the room is available' do
+      it 'returns the room booking value' do
+        # Arrange
+        cities = %w[Itapetininga Sorocaba Camboriú]
+        states = %w[SP SP SC]
+        guesthouses_names = %w[Nascer\ do\ Sol Sorocabana Praiana]
+        user_names = %w[Joao Marlene Cesar]
+        guesthouse =  {
+          0 => '', 1 => ''
+        }
+        user = {
+          0 => '', 1 => ''
+        }
+        guesthouse_owner = {
+          0 => '', 1 => ''
+        }
 
-      PaymentMethod.create!(method: 'credit_card')
-      PaymentMethod.create!(method: 'debit_card')
-      PaymentMethod.create!(method: 'pix')
+        PaymentMethod.create!(method: 'credit_card')
+        PaymentMethod.create!(method: 'debit_card')
+        PaymentMethod.create!(method: 'pix')
 
-      guesthouses_names.each_with_index do |name, index|
-        user[index] = User.create!(name: user_names[index], email: "#{user_names[index].downcase}@email.com",
-                                   password: 'password', role: 1)
-        guesthouse_owner[index] = user[index].build_guesthouse_owner
-        guesthouse[index] = guesthouse_owner[index].build_guesthouse(corporate_name: "#{name} LTDA.",
-                                                                     brand_name: "Pousada #{name}",
-                                                                     registration_code: "#{index}47032102000152",
-                                                                     phone_number: "#{index}1598308183",
-                                                                     email: "contato@#{name.downcase.gsub(" ", "")}.com.br",
-                                                                     description: "Descrição da Pousada #{name}",
-                                                                     pets: index.odd? ? true : false,
-                                                                     use_policy: 'Não é permitido fumar nas dependências da pousada',
-                                                                     checkin_hour: '14:00', checkout_hour: '12:00',
-                                                                     active: index.even? ? true : false)
+        guesthouses_names.each_with_index do |name, index|
+          user[index] = User.create!(name: user_names[index], email: "#{user_names[index].downcase}@email.com",
+                                     password: 'password', role: 1)
+          guesthouse_owner[index] = user[index].build_guesthouse_owner
+          guesthouse[index] = guesthouse_owner[index].build_guesthouse(corporate_name: "#{name} LTDA.",
+                                                                       brand_name: "Pousada #{name}",
+                                                                       registration_code: "#{index}47032102000152",
+                                                                       phone_number: "#{index}1598308183",
+                                                                       email: "contato@#{name.downcase.gsub(" ", "")}.com.br",
+                                                                       description: "Descrição da Pousada #{name}",
+                                                                       pets: index.odd? ? true : false,
+                                                                       use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                                       checkin_hour: '14:00', checkout_hour: '12:00',
+                                                                       active: index.even? ? true : false)
 
-        guesthouse[index].build_address(street: index.odd? ? "Rua #{index},  #{index}000" : "Avenida #{index}, #{index}000",
-                                        neighborhood: index.even? ? "Bairro #{index}" : "Centro #{index}",
-                                        city: cities[index], state: states[index], postal_code: "#{index}1001-000")
+          guesthouse[index].build_address(street: index.odd? ? "Rua #{index},  #{index}000" : "Avenida #{index}, #{index}000",
+                                          neighborhood: index.even? ? "Bairro #{index}" : "Centro #{index}",
+                                          city: cities[index], state: states[index], postal_code: "#{index}1001-000")
 
-        guesthouse[index].payment_methods = PaymentMethod.all
-        guesthouse[index].save!
+          guesthouse[index].payment_methods = PaymentMethod.all
+          guesthouse[index].save!
+
+          guesthouse[index].rooms.create!([{ name: "Quarto Aquarela #{index}",
+                                             description: 'Quarto com vista para a serra',
+                                             size: 30, max_people: 2, daily_rate: 100,
+                                             bathroom: true, balcony: true,
+                                             air_conditioning: index.even? ? true : false,
+                                             tv: index.even? ? true : false, wardrobe: true,
+                                             safe: true, accessible: index.even? ? true : false,
+                                             available: true },
+                                           { name: "Quarto Oceano #{index}",
+                                             description: 'Quarto com vista para o mar',
+                                             size: 40, max_people: 3, daily_rate: 200,
+                                             bathroom: true, balcony: true,
+                                             air_conditioning: index == 0 ? false : true,
+                                             tv: index == 1 ? false : true, wardrobe: true,
+                                             safe: true, accessible: index == 0 ? false : true,
+                                             available: false },
+                                           { name: "Quarto Céu #{index}",
+                                             description: 'Quarto com vista para a montanha',
+                                             size: 50, max_people: 4, daily_rate: 300,
+                                             bathroom: true, balcony: true,
+                                             air_conditioning: index.even? ? true : false,
+                                             tv: index.even? ? true : false, wardrobe: true,
+                                             safe: true, accessible: index.even? ? true : false,
+                                             available: true }])
+        end
+
+        # Act
+        query = { check_in_date: 1.days.from_now, check_out_date: 2.days.from_now, number_of_guests: 2 }
+        url_query = query.to_query
+        get "/api/v1/guesthouses/#{guesthouse[0].id}/rooms/#{guesthouse[0].rooms[0].id}/availability?#{url_query}"
+
+        # Assert
+        expect(response).to have_http_status(200)
+        expect(response.content_type).to include('application/json')
+
+        parsed_body = JSON.parse(response.body, symbolize_names: true)
+        expect(parsed_body[:available]).to eq(true)
+        expect(parsed_body[:total_price]).to eq("100.0")
       end
-
-      # Act
-      get "/api/v1/guesthouses/#{guesthouse[0].id}"
-
-      # Assert
-      expect(response).to have_http_status(200)
-      expect(response.content_type).to include('application/json')
-
-      parsed_body = JSON.parse(response.body, symbolize_names: true)
-      expect(parsed_body[:brand_name]).to eq('Pousada Nascer do Sol')
-      expect(parsed_body[:phone_number]).to eq('01598308183')
-      expect(parsed_body[:email]).to eq('contato@nascerdosol.com.br')
-      expect(parsed_body[:description]).to eq('Descrição da Pousada Nascer do Sol')
-      expect(parsed_body[:pets]).to eq(false)
-      expect(parsed_body[:use_policy]).to eq('Não é permitido fumar nas dependências da pousada')
-      expect(DateTime.parse(parsed_body[:checkin_hour])).to eq(guesthouse[0].checkin_hour)
-      expect(DateTime.parse(parsed_body[:checkout_hour])).to eq(guesthouse[0].checkout_hour)
-      expect(parsed_body[:active]).to eq(true)
-      expect(parsed_body[:address][:street]).to eq('Avenida 0, 0000')
-      expect(parsed_body[:address][:neighborhood]).to eq('Bairro 0')
-      expect(parsed_body[:address][:city]).to eq('Itapetininga')
-      expect(parsed_body[:address][:state]).to eq('SP')
-      expect(parsed_body[:address][:postal_code]).to eq('01001-000')
-      expect(parsed_body[:payment_methods][0][:method]).to eq('credit_card')
-      expect(parsed_body[:payment_methods][1][:method]).to eq('debit_card')
-      expect(parsed_body[:payment_methods][2][:method]).to eq('pix')
-      expect(parsed_body[:average_rating]).to eq(guesthouse[0].average_rating)
-      expect(parsed_body.keys).to_not include(:created_at)
-      expect(parsed_body.keys).to_not include(:updated_at)
-      expect(parsed_body.keys).to_not include(:corporate_name)
-      expect(parsed_body.keys).to_not include(:registration_code)
     end
 
     it 'fails if it is not found' do
