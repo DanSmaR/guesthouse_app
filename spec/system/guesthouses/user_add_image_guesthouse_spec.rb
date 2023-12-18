ecb1b45a1bc1de4c2f09437a26e2eaad5c32b7e1
Danilo Martins <dansm86@outlook.com>
2023-12-18 15:18:02 -0300

    test:user add wrong image type

--- spec/system/guesthouses/user_add_image_guesthouse_spec.rb
+++ spec/system/guesthouses/user_add_image_guesthouse_spec.rb
@@ -39,5 +39,38 @@ describe 'User add image to guesthouse' do
     (1..4).each { |id| expect(page).to have_css("img[src*='pousada_#{id}.jpg']") }
   end
 
+  it 'gets an error message if the file type is not "jpeg" or "png"' do
+    # Arrange
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
+    guesthouse_owner = user.build_guesthouse_owner
+    guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
+                                                   brand_name: 'Pousada Nascer do Sol',
+                                                   registration_code: '47032102000152',
+                                                   phone_number: '15983081833',
+                                                   email: 'contato@nascerdosol.com.br',
+                                                   description: 'Pousada com vista linda para a serra',
+                                                   pets: true,
+                                                   use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                   checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+    guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
+                             city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
 
+    PaymentMethod.create!(method: 'credit_card')
+    PaymentMethod.create!(method: 'debit_card')
+    PaymentMethod.create!(method: 'pix')
+
+    guesthouse.payment_methods = PaymentMethod.all
+    guesthouse.save!
+
+    # Act
+    login_as(user)
+    visit edit_guesthouse_path(guesthouse)
+    attach_file('Imagens', "app/assets/images/guesthouse/wrong.avif")
+
+    # Assert
+    click_button 'Atualizar Pousada'
+
+    expect(page).to have_content 'Fotos'
+    expect(page).to have_content 'Somente extensões png e jpeg são permitidas'
+  end
 end
