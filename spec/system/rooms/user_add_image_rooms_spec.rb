a5b62783e319699e83e4b29b8d08bd322a903935
Danilo Martins <dansm86@outlook.com>
2023-12-20 09:22:52 -0300

    test:feat:keep room images after updating
    keep the previous images if more or no images are added

--- spec/system/rooms/user_add_image_rooms_spec.rb
+++ spec/system/rooms/user_add_image_rooms_spec.rb
@@ -93,84 +93,104 @@ describe 'User add image to rooms' do
 
     expect(page).to have_content 'Somente extensões png e jpeg são permitidas'
   end
-  #
-  # it 'keeps the previous images even if no image is uploaded in current edition' do
-  #   # Arrange
-  #   user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
-  #   guesthouse_owner = user.build_guesthouse_owner
-  #   guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
-  #                                                  brand_name: 'Pousada Nascer do Sol',
-  #                                                  registration_code: '47032102000152',
-  #                                                  phone_number: '15983081833',
-  #                                                  email: 'contato@nascerdosol.com.br',
-  #                                                  description: 'Pousada com vista linda para a serra',
-  #                                                  pets: true,
-  #                                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
-  #                                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
-  #   guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
-  #                            city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
-  #
-  #   PaymentMethod.create!(method: 'credit_card')
-  #   PaymentMethod.create!(method: 'debit_card')
-  #   PaymentMethod.create!(method: 'pix')
-  #
-  #   guesthouse.payment_methods = PaymentMethod.all
-  #   guesthouse.save!
-  #   (1..3).map do |id|
-  #     guesthouse.images.attach(
-  #       io: File.open("app/assets/images/guesthouse/pousada_#{id}.jpg"),
-  #       filename: "pousada_#{id}.jpg",
-  #       content_type: "image/jpeg"
-  #     )
-  #   end
-  #
-  #   # Act
-  #   login_as(user)
-  #   visit edit_guesthouse_path(guesthouse)
-  #   click_button 'Atualizar Pousada'
-  #
-  #   # Assert
-  #   expect(page).to have_content 'Fotos'
-  #   (1..3).each { |id| expect(page).to have_css("img[src*='pousada_#{id}.jpg']") }
-  # end
-  #
-  # it 'add to the previous images if one more is uploaded in current edition' do
-  #   # Arrange
-  #   user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
-  #   guesthouse_owner = user.build_guesthouse_owner
-  #   guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
-  #                                                  brand_name: 'Pousada Nascer do Sol',
-  #                                                  registration_code: '47032102000152',
-  #                                                  phone_number: '15983081833',
-  #                                                  email: 'contato@nascerdosol.com.br',
-  #                                                  description: 'Pousada com vista linda para a serra',
-  #                                                  pets: true,
-  #                                                  use_policy: 'Não é permitido fumar nas dependências da pousada',
-  #                                                  checkin_hour: '14:00', checkout_hour: '12:00', active: true)
-  #   guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
-  #                            city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
-  #
-  #   PaymentMethod.create!(method: 'credit_card')
-  #   PaymentMethod.create!(method: 'debit_card')
-  #   PaymentMethod.create!(method: 'pix')
-  #
-  #   guesthouse.payment_methods = PaymentMethod.all
-  #   guesthouse.save!
-  #   (1..3).map do |id|
-  #     guesthouse.images.attach(
-  #       io: File.open("app/assets/images/guesthouse/pousada_#{id}.jpg"),
-  #       filename: "pousada_#{id}.jpg",
-  #       content_type: "image/jpeg"
-  #     )
-  #   end
-  #
-  #   # Act
-  #   login_as(user)
-  #   visit edit_guesthouse_path(guesthouse)
-  #   attach_file('Imagens', "app/assets/images/guesthouse/pousada_4.jpg")
-  #   click_button 'Atualizar Pousada'
-  #
-  #   # Assert
-  #   (1..4).each { |id| expect(page).to have_css("img[src*='pousada_#{id}.jpg']") }
-  # end
+
+  it 'keeps the previous images even if no image is uploaded in current edition' do
+    # Arrange
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
+    guesthouse_owner = user.build_guesthouse_owner
+    guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
+                                                   brand_name: 'Pousada Nascer do Sol',
+                                                   registration_code: '47032102000152',
+                                                   phone_number: '15983081833',
+                                                   email: 'contato@nascerdosol.com.br',
+                                                   description: 'Pousada com vista linda para a serra',
+                                                   pets: true,
+                                                   use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                   checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+    guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
+                             city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
+
+    PaymentMethod.create!(method: 'credit_card')
+    PaymentMethod.create!(method: 'debit_card')
+    PaymentMethod.create!(method: 'pix')
+
+    guesthouse.payment_methods = PaymentMethod.all
+    guesthouse.save!
+
+    guesthouse.rooms.create!([{ name: 'Quarto Primavera', description: 'Quarto com vista para a serra', size: 30,
+                                max_people: 2, daily_rate: 100, bathroom: true, balcony: true,
+                                air_conditioning: true, tv: true, wardrobe: true, safe: true, accessible: true,
+                                available: true },
+                              { name: 'Quarto Verão', description: 'Quarto com vista para o mar', size: 30,
+                                max_people: 2, daily_rate: 100, bathroom: true, balcony: true,
+                                air_conditioning: true, tv: true, wardrobe: true, safe: true, accessible: true,
+                                available: false }])
+
+    (1..3).map do |id|
+      guesthouse.rooms.first&.images&.attach(
+        io: File.open("app/assets/images/rooms/room_#{id}.jpg"),
+        filename: "room_#{id}.jpg",
+        content_type: "image/jpeg"
+      )
+    end
+
+    # Act
+    login_as(user)
+    visit edit_guesthouse_room_path(guesthouse, guesthouse.rooms.first)
+    click_button 'Atualizar Quarto'
+
+    # Assert
+    expect(page).to have_content 'Fotos'
+    (1..3).each { |id| expect(page).to have_css("img[src*='room_#{id}.jpg']") }
+  end
+
+  it 'add to the previous images if one more is uploaded in current edition' do
+    # Arrange
+    user = User.create!(name: 'João', email: 'joao@email.com', password: 'password', role: 1)
+    guesthouse_owner = user.build_guesthouse_owner
+    guesthouse = guesthouse_owner.build_guesthouse(corporate_name: 'Pousada Nascer do Sol LTDA.',
+                                                   brand_name: 'Pousada Nascer do Sol',
+                                                   registration_code: '47032102000152',
+                                                   phone_number: '15983081833',
+                                                   email: 'contato@nascerdosol.com.br',
+                                                   description: 'Pousada com vista linda para a serra',
+                                                   pets: true,
+                                                   use_policy: 'Não é permitido fumar nas dependências da pousada',
+                                                   checkin_hour: '14:00', checkout_hour: '12:00', active: true)
+    guesthouse.build_address(street: 'Rua das Flores, 1000', neighborhood: 'Vila Belo Horizonte' ,
+                             city: 'Itapetininga', state: 'SP', postal_code: '01001-000')
+
+    PaymentMethod.create!(method: 'credit_card')
+    PaymentMethod.create!(method: 'debit_card')
+    PaymentMethod.create!(method: 'pix')
+
+    guesthouse.payment_methods = PaymentMethod.all
+    guesthouse.save!
+
+    guesthouse.rooms.create!([{ name: 'Quarto Primavera', description: 'Quarto com vista para a serra', size: 30,
+                                max_people: 2, daily_rate: 100, bathroom: true, balcony: true,
+                                air_conditioning: true, tv: true, wardrobe: true, safe: true, accessible: true,
+                                available: true },
+                              { name: 'Quarto Verão', description: 'Quarto com vista para o mar', size: 30,
+                                max_people: 2, daily_rate: 100, bathroom: true, balcony: true,
+                                air_conditioning: true, tv: true, wardrobe: true, safe: true, accessible: true,
+                                available: false }])
+
+    (1..2).map do |id|
+      guesthouse.rooms.first&.images&.attach(
+        io: File.open("app/assets/images/rooms/room_#{id}.jpg"),
+        filename: "room_#{id}.jpg",
+        content_type: "image/jpeg"
+      )
+    end
+
+    # Act
+    login_as(user)
+    visit edit_guesthouse_room_path(guesthouse, guesthouse.rooms.first)
+    attach_file('Imagens', "app/assets/images/rooms/room_3.jpg")
+    click_button 'Atualizar Quarto'
+
+    # Assert
+    (1..3).each { |id| expect(page).to have_css("img[src*='room_#{id}.jpg']") }
+  end
 end
